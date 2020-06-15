16116038729380f43316fe23a529130512c1f720
EmanAALK <emanalkandarii@gmail.com>
2020-06-15 18:16:59 +0300

    your message here.task2

--- story.js
+++ story.js
@@ -1,7 +1,12 @@
 // Define the name here on line 2
-
+let name = "Eman";
 // Define the age here on line 4
-
+let age = 26;
 // Define the hobby here on line 6
-
+let hobby = "Writting";
 // Print the story here on line 8
+console.log(
+  `Ms. ${name}, in 10 years, you will be ${
+    age + 10
+  } years old. By then you'll have decades of ${hobby} practice`
+);
